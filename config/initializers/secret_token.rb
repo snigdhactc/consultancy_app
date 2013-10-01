29ea16cf311e422169484fb52849aa7728ba26e9
Snigdha <singdhactc@gmail.com>
2013-10-01 08:29:35 +0530

    Changes in layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Consultancy::Application.config.secret_key_base = '0dd92d38ed24f40e25512bd7ab11ccd6a79034161f8963e07e512fabf2315b8819709dd0a6ef59996fd498afb1620890dda03202c20aa94e10051a8c05575268'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Consultancy::Application.config.session_store :cookie_store, key: '_consultancy_session'
